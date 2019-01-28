4be75dd877096830ae4dd0a6153ecfc167c1b3d8
mcbeem <mmcbee@gmail.com>
2019-01-28 09:07:08 -0500

    Update plot_perf_by_cutoff_method.R
    add attribution to comments

--- R/plot_perf_by_cutoff_method.R
+++ R/plot_perf_by_cutoff_method.R
@@ -8,6 +8,9 @@
 #'
 #' @export
 
+# the areColors function is courtesy of StackOverflow user Josh O'Brien
+# https://stackoverflow.com/questions/13289009/check-if-character-string-is-a-valid-color-representation
+
 areColors <- function(x) {
   sapply(x, function(X) {
     tryCatch(is.matrix(col2rgb(X)),
